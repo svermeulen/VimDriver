9fe84abe5775b60c392e952580ac8555c11f13f2
svermeulen <sfvermeulen@gmail.com>
2014-09-06 18:33:17 -0300

    More

---
+++ tests/Tests.py
@@ -0,0 +1,27 @@
+
+from Logger import *
+
+Log('hi')
+
+#import sys
+#import os
+
+#from SystemHelper import SystemHelper
+#from Logger import Logger
+
+##vim --servername FOO --remote-expr feedkeys('k')
+
+#class Runner:
+    #def __init__(self, logger, sys):
+        #self.logger = logger
+        #self.sys = sys
+
+    #def Run(self):
+        #self.logger.Log("hi")
+
+#if __name__ == '__main__':
+    #logger = Logger(sys.stdout, True)
+    #sysHelper = SystemHelper({}, logger)
+    #runner = Runner(logger, sysHelper)
+    #runner.Run()
+
